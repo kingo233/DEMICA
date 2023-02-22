7eb741145e6ead33f3831f111a1d4197c7e4ecec
jiangnan <kingo233@outlook.com>
2023-02-22 15:38:11 +0800

    debug

--- decalib/trainer.py
+++ decalib/trainer.py
@@ -203,6 +203,8 @@ class Trainer(object):
             ground_flame_para = batch['flame']
             exp_code = ground_flame_para['expression_params']
             exp_code = torch.cat([exp_code,exp_code,exp_code],dim=0)
+            print(f'expcode.shape = {expcode.shape}')
+            print(f"shape param = {ground_flame_para['shape_params'].shape}")
             ground_flame_verts, landmarks2d_, landmarks3d_ = self.deca.flame(
                 shape_params=ground_flame_para['shape_params'], # 300
                 expression_params=exp_code)
