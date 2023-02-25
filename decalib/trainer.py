5c56022b8d2682b61d3faab3e791b64a64ed829e
kingo233 <kingo233@outlook.com>
2023-02-25 15:35:52 +0800

    tuning:min_lr

--- decalib/trainer.py
+++ decalib/trainer.py
@@ -217,7 +217,7 @@ class Trainer(object):
             # ground_flame_verts = ground_flame_verts.reshape(real_batch_size,5023*3)
             # pred_flame_verts = pred_flame_verts.permute(1,0,2)
             
-            losses['flame'] = (pred_flame_verts - ground_flame_verts).abs().mean() * 1000
+            losses['flame'] = (pred_flame_verts - ground_flame_verts).abs().mean()
 
             if self.cfg.model.jaw_type == 'euler':
                 # import ipdb; ipdb.set_trace()
